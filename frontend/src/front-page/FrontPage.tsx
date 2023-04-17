73a58ea1fd8332c741dcb0cbb500e62d03182ae5
Bramstorp <bramstorpmathias@gmail.com>
2023-04-17 18:08:11 +0200

    update to frontpage, added icons, wip for returnpage.

--- frontend/src/front-page/FrontPage.tsx
+++ frontend/src/front-page/FrontPage.tsx
@@ -1,23 +1,27 @@
 import React, { FunctionComponent } from "react";
 import { Link } from "react-router-dom";
+import { FiPackage } from "react-icons/fi";
+import { FiShoppingCart } from "react-icons/fi";
 
 export const FrontPage: FunctionComponent = () => {
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center">
       <h1 className="text-7xl mb-20">SELVBETJENING</h1>
       <div className="flex items-center justify-center">
-        <a
-          href="#"
-          className="bg-gray-300 hover:bg-sky-700 text-white font-bold rounded-lg border shadow-lg m-2 p-10"
+        <Link
+          to="return"
+          className="bg-gray-500 hover:bg-sky-700 rounded-lg border shadow-lg m-2 p-10"
         >
-          <Link to="return">Retur vare</Link>
-        </a>
-        <a
-          href="#"
-          className="bg-gray-300 hover:bg-sky-700 text-white font-bold rounded-lg border shadow-lg m-2 p-10"
+          <p className="text-white font-bold">Retur vare</p>
+          <FiPackage className="m-auto mt-2 text-white w-10 h-10" />
+        </Link>
+        <Link
+          to="click-and-collect"
+          className="bg-gray-500 hover:bg-sky-700 rounded-lg border shadow-lg m-2 p-10"
         >
-          <Link to="click-and-collect">Click and Collect</Link>
-        </a>
+          <p className="text-white font-bold">Click and Collect</p>
+          <FiShoppingCart className="m-auto mt-2 text-white w-10 h-10" />
+        </Link>
       </div>
     </div>
   );
--- frontend/src/return/ReturnPage.tsx
+++ frontend/src/return/ReturnPage.tsx
@@ -3,8 +3,11 @@ import { useRouteError } from "react-router-dom";
 
 export const ReturnPage: FunctionComponent = () => {
   return (
-    <div>
-      <p>Return</p>
+    <div className="h-screen w-full flex flex-col justify-center items-center">
+      <h1 className="text-7xl  ">SELVBETJENING</h1>
+      <div className="flex items-center justify-center">
+        <p>test</p>
+      </div>
     </div>
   );
 };
