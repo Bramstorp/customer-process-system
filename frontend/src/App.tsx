561fe1f5fcf424219131b30e73556078ec213f63
Mathias Bramstorp <bramstorpmathias@gmail.com>
2023-04-17 10:04:23 +0200

    eslint, prettier, tsconfig update and added tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <p>Tester</p>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
