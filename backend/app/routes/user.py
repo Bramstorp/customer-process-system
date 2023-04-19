de87ed2177e064ddcd4c5953e6344e227f9af54a
Bramstorp <bramstorpmathias@gmail.com>
2023-04-19 12:00:30 +0200

    user route update.

--- backend/app/routes/user.py
+++ backend/app/routes/user.py
@@ -11,27 +11,32 @@ user_router = APIRouter()
 auth_handler = AuthHandler()
 
 
-@user_router.post('/registration', status_code=201, tags=['users'], description='Register new user')
-def register(user: UserInput):
+@user_router.post('/signup', status_code=201, tags=['users'], description='Register new user')
+def signup(user: UserInput):
     users = select_all_users()
     if any(x.username == user.username for x in users):
         raise HTTPException(status_code=400, detail='Username is taken')
+    
     hashed_pwd = auth_handler.get_password_hash(user.password)
-    u = User(username=user.username, password=hashed_pwd)
-    session.add(u)
+    created_user = User(username=user.username, password=hashed_pwd)
+    session.add(created_user)
     session.commit()
+    
     return JSONResponse(status_code=HTTP_201_CREATED)
 
 
 @user_router.post('/login', tags=['users'])
 def login(user: UserLogin):
     user_found = find_user(user.username)
+    
     if not user_found:
         raise HTTPException(status_code=401, detail='Invalid username and/or password')
     verified = auth_handler.verify_password(user.password, user_found.password)
+    
     if not verified:
         raise HTTPException(status_code=401, detail='Invalid username and/or password')
     token = auth_handler.encode_token(user_found.username)
+    
     return {'token': token}
 
 
