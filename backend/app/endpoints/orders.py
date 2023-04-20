182e5d270fb1a8c45972969fb1481df9123aed8e
Bramstorp <bramstorpmathias@gmail.com>
2023-04-20 22:34:41 +0200

    naming update.

--- backend/app/endpoints/orders.py
+++ backend/app/endpoints/orders.py
@@ -2,37 +2,26 @@ from fastapi import APIRouter, Depends
 from starlette.responses import JSONResponse
 from starlette.status import HTTP_404_NOT_FOUND, HTTP_401_UNAUTHORIZED, HTTP_204_NO_CONTENT
 from fastapi.encoders import jsonable_encoder
-
+from sqlmodel import select
 from app.db.database import session
-from app.models.orders import Order, Orderline, OrderCreate, OrderlineCreate, OrderRead
+from app.models.orders import Order, Orderline, OrderCreate, OrderRead
 
 from app.routes.user import auth_handler
 
 order_routes = APIRouter()
 
 @order_routes.post('/create-order', tags=['orders'], status_code=201, description='Create new order')
-def create_order(order: OrderCreate, order_line: OrderlineCreate):        
-    db_order = Order.from_orm(order)
-    
+def create_order(order: OrderCreate):        
+    db_order = Orderline.from_orm(order)
     session.add(db_order)
     session.commit()
     session.refresh(db_order)
-
     return db_order
 
-@order_routes.post('/order/{order_id}',response_model=OrderRead, tags=['orders'], status_code=201, description='Create new order')
+@order_routes.post('/order/{order_id}', response_model=OrderRead, tags=['orders'], status_code=201, description='Create new order')
 def get_order(order_id: int):
     order = session.get(Order, order_id)
     if not order:
         return JSONResponse(status_code=HTTP_404_NOT_FOUND, content='Order not found')
     
     return order
-
-@order_routes.post('/create-orderline', tags=['orderlines'], status_code=201, description='Create new orderline')
-def create_orderline(orderline: OrderlineCreate):        
-    db_orderline = Orderline.from_orm(orderline)
-    session.add(db_orderline)
-    session.commit()
-    session.refresh(db_orderline)
-
-    return db_orderline
--- backend/app/models/orders.py
+++ backend/app/models/orders.py
@@ -12,7 +12,6 @@ class Order(OrderBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     customer_id: Optional[int] = Field(default=None, foreign_key='customers.id')
     customer: Optional[Customers] = Relationship()
-    orderlines: List["Orderline"] = Relationship(back_populates="order", sa_relationship=RelationshipProperty("Orderline", primaryjoin="Order.id == Orderline.order_id", uselist=True))
     
 class OrderCreate(OrderBase):
     pass
@@ -20,26 +19,8 @@ class OrderCreate(OrderBase):
 class OrderRead(OrderBase):
     pass
 
-
-class OrderlineBase(SQLModel):
-    productnumber: str
-    productname: str
-    amount: int
-    unitprice: float
-    totalprice: float
-
-class Orderline(OrderlineBase, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    order: List[Order] = Relationship(back_populates="orderline", sa_relationship=RelationshipProperty("Order", primaryjoin="foreign(Order.orderline_id) == Order.id", uselist=False))
-    productnumber: str
-    productname: str
-    amount: int
-    unitprice: float
-    totalprice: float
-
-class OrderlineCreate(OrderlineBase):
-    pass
-    
+class OrderReadWithOrderlines(OrderRead):
+    orderlines: List["Orderline"] = []
 
 class GetOrder(SQLModel):
     order_id: int
