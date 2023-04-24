a23b1ea08a76c3fa7e35854873fe83e53e619e14
Bramstorp <bramstorpmathias@gmail.com>
2023-04-24 20:11:47 +0200

    navbar update

--- frontend/src/shared/Navbar.tsx
+++ frontend/src/shared/Navbar.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const Navbar: FunctionComponent = () => {
   return (
     <nav className="border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-      <div className="flex flex-wrap items-center justify-between p-4">
+      <div className="flex flex-wrap items-center justify-start p-4">
         <img
           src="https://www.anmeld-haandvaerker.dk/sites/default/files/styles/sponsor_logo_large/public/sponsor_partner_logo/ao-logo-png_0.png?itok=GCxIO0SO"
           className="h-8 mr-3"
@@ -13,9 +13,6 @@ export const Navbar: FunctionComponent = () => {
         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
           SELVBETJENING
         </span>
-        <Link className="text-blue-500" to="/config">
-          Config
-        </Link>
       </div>
     </nav>
   );
