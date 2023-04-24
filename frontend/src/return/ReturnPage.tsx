053032ba621c4d8a3331fac25cf0cede8ec8bc6b
Bramstorp <bramstorpmathias@gmail.com>
2023-04-24 14:24:08 +0200

    final ordernumber finder.

--- frontend/src/return/ReturnPage.tsx
+++ frontend/src/return/ReturnPage.tsx
@@ -1,7 +1,40 @@
-import React, { FunctionComponent } from "react";
-import { useRouteError } from "react-router-dom";
+import React, { FunctionComponent, useState } from "react";
+
+const btnLayout = [
+  ["7", "8", "9", "SLET"],
+  ["4", "5", "6", "ENTER"],
+  ["1", "2", "3"],
+  ["0"],
+];
 
 export const ReturnPage: FunctionComponent = () => {
+  let [ordernr, setOrdernr] = useState({
+    sign: "",
+    num: "",
+    res: "",
+  });
+
+  const numClick = (e: any) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    setOrdernr({
+      ...ordernr,
+      num: ordernr.num + value,
+    });
+  };
+
+  const enter = () => {
+    console.log("enter");
+  };
+
+  const removeLast = () => {
+    setOrdernr({
+      ...ordernr,
+      num: ordernr.num.slice(0, -1),
+    });
+  };
+
   return (
     <div className="pt-4 flex flex-col items-center justify-center">
       <h1 className="text-3xl mt-10 mb-10">
@@ -14,24 +47,34 @@ export const ReturnPage: FunctionComponent = () => {
         <input
           type="number"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder=""
+          placeholder="ordrenr"
+          value={ordernr.num}
           required
         />
         <div className="grid grid-cols-4 text-black text-md text-center font-bold leading-6 pt-4 gap-2">
-          <div className="bg-slate-100 p-10 rounded">7</div>
-          <div className="bg-slate-100 p-10 rounded">8</div>
-          <div className="bg-slate-100 p-10 rounded">9</div>
-          <div className="bg-slate-100 p-10 rounded">Slet</div>
-          <div className="bg-slate-100 p-10 rounded">4</div>
-          <div className="bg-slate-100 p-10 rounded">5</div>
-          <div className="bg-slate-100 p-10 rounded">6</div>
-          <div className="p-4 rounded bg-slate-100 grid place-content-center row-span-3">
-            Enter
-          </div>
-          <div className="bg-slate-100 p-10 rounded">1</div>
-          <div className="bg-slate-100 p-10 rounded">2</div>
-          <div className="bg-slate-100 p-10 rounded">3</div>
-          <div className="bg-slate-100 col-span-3 p-10 rounded">0</div>
+          {btnLayout.flat().map((btn, i) => {
+            return (
+              <a
+                className={`bg-slate-100 p-10 rounded grid place-content-center ${
+                  btn === "0"
+                    ? "col-span-3"
+                    : btn === "ENTER"
+                    ? "row-span-3"
+                    : ""
+                }`}
+                key={i}
+                onClick={
+                  btn === "SLET"
+                    ? removeLast
+                    : btn === "ENTER"
+                    ? enter
+                    : numClick
+                }
+              >
+                {btn}
+              </a>
+            );
+          })}
         </div>
       </div>
     </div>
