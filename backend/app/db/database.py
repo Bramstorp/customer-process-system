9c31c8d0caf5edfb370dac4d5b29a843c5028f8f
Bramstorp <bramstorpmathias@gmail.com>
2023-05-02 00:01:38 +0200

    env file.

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -4,7 +4,16 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "myuser")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "mypassword")
+POSTGRES_SERVER = os.getenv("POSTGRES_SERVER", "localhost")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "customer_process_system")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL is None:
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 engine = create_engine(DATABASE_URL)
 session = Session(bind=engine)
