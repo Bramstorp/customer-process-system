5a41cad8cc9f4e6d93c71c7df72a17bae64d1454
Bramstorp <bramstorpmathias@gmail.com>
2023-05-08 15:37:18 +0200

    print label with qrcode.

--- backend/app/utils/zgl.py
+++ backend/app/utils/zgl.py
@@ -1,16 +1,25 @@
 import zebra
+import qrcode
+import socket
 
-
-def print_label(ip_address):
-    printer = zebra.Printer(ip_address)
-
-    zpl = """
+def print_label(ip_address, kode=""):
+    zpl = f"""
     ^XA
     ^FO50,50
     ^A0N,50,50
-    ^FDHello, World!^FS
-    ^XZ
+    ^FD{kode}^FS
     """
 
-    printer.send(zpl)
-    printer.close()
+    qr = qrcode.QRCode(version=1, box_size=10, border=1)
+    qr.add_data(kode)
+    qr.make(fit=True)
+
+    zpl += f"^FO50,100\n^BQN,2,10\n^FDLA,{qr.get_png_as_base64_str(scale=5)}^FS\n"
+
+    zpl += "^XZ\n"
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip_address, 9100))
+
+    s.send(zpl.encode('utf-8'))
+    s.close()
