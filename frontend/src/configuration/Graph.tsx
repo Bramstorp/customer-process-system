4cf199064121b630c193a9d14ffb25146956977e
Bramstorp <bramstorpmathias@gmail.com>
2023-05-11 17:03:29 +0200

    added y axes.

--- frontend/src/configuration/Graph.tsx
+++ frontend/src/configuration/Graph.tsx
@@ -1,6 +1,4 @@
-import React, { FunctionComponent, useContext, useState } from "react";
-import { CompanyContext } from "../service/company/company.context";
-import { ICase } from "../types/cases.type";
+import React, { FunctionComponent } from "react";
 
 interface CasesByHour {
   hour: number;
@@ -13,17 +11,32 @@ interface IProps {
 
 export const Graph: FunctionComponent<IProps> = ({ data }) => {
   const maxCount = Math.max(...data.map((item) => item.count));
+  const minCount = Math.min(...data.map((item) => item.count));
+
+  const yTicks = [maxCount, Math.round((maxCount + minCount) / 2), minCount];
 
   return (
     <div className="rounded-lg border border-gray-200 p-8">
-      <h4>Graph customers visit by hour</h4>
+      <h4 className="mb-4">Graph customers visit by hour</h4>
       <div className="flex items-end h-60">
-        {data.map((item) => (
-          <div key={item.hour} className="flex flex-col justify-end items-center h-full w-1/5">
-            <div className="w-8 bg-blue-400" style={{ height: `${(item.count / maxCount) * 100}%` }}></div>
-            <span className="text-xs">{item.hour}</span>
-          </div>
-        ))}
+        <div className="flex flex-col h-full w-1 justify-between">
+          {yTicks.map((tick) => (
+            <span key={tick} className="text-xs">
+              {tick}
+            </span>
+          ))}
+        </div>
+        <div className="flex items-end h-full w-5/6">
+          {data.map((item) => (
+            <div key={item.hour} className="relative flex flex-col justify-end items-center h-full w-1/5">
+              <div
+                className={`w-8 ${maxCount == item.count ? "bg-blue-500" : "bg-blue-400"}`}
+                style={{ height: `${(item.count / maxCount) * 100}%` }}
+              ></div>
+              <span className="text-xs">{item.hour}</span>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
