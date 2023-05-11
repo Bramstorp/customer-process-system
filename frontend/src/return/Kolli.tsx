9c50cf8cf8350e297423b22320fb4c286c218879
Bramstorp <bramstorpmathias@gmail.com>
2023-05-11 14:31:42 +0200

    missing orderid.

--- frontend/src/return/Kolli.tsx
+++ frontend/src/return/Kolli.tsx
@@ -32,7 +32,7 @@ export const Kolli: FunctionComponent = () => {
   return (
     <div className="pt-4 flex flex-col items-center justify-center">
       <h1 className="text-3xl mt-10 mb-10">Vælg kolli antal</h1>
-      <p className="text-2xl">Ordrenummer: {location.state.id}</p>
+      <p className="text-2xl">Ordrenummer: {location.state.order.id}</p>
       <p className="text-2xl">Navn: {location.state.customer.name}</p>
       <Numpad label={"Kolli antal"} placeholder={"indsæt kolliantal"} onClick={(kolli) => createReturnCase(kolli)} />
     </div>
